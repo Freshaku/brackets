ad7dcf4664084619003abc9ecda72c29373a16a8
Freshaku <52348428+Freshaku@users.noreply.github.com>
2020-10-23 15:08:18 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let bracketsNew = new Map(bracketsConfig);
+  let stack = [];
+   for (let i = 0; i< str.length; i++)
+   {
+     str[i] === bracketsNew.get(stack[stack.length-1]) ? stack.pop() : stack.push(str[i]);
+   }
+   return stack.length === 0;
 }
